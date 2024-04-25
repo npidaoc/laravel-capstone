8c09c2dd3d7a1afe1f90f1a87d04442c5a247f1f
npidaoc <npidaoc@gmail.com>
2024-04-26 00:43:46 +0800

    changes

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png';
-import menu_icon from '../../assets/menu-icon.png';
+import menu_icon from '../../assets/menu_icon.png';
 import { Link } from 'react-scroll';
-import { useNavigate } from 'react-router-dom'; // Import useNavigate for routing
+import { NavLink, useNavigate } from 'react-router-dom';
 
 function Navbar() {
     const [sticky, setSticky] = useState(false);
-    const navigate = useNavigate(); // Initialize useNavigate hook for programmatic navigation
+    const navigate = useNavigate();
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
@@ -20,6 +20,7 @@ function Navbar() {
     const toggleMenu = () => {
         setMobileMenu(prevState => !prevState);
     };
+
 
     return (
         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
@@ -72,128 +73,3 @@ export default Navbar;
 
 
 
-// import React, { useEffect, useState } from 'react';
-// import './Navbar.css';
-// import logo from '../../assets/logo.png';
-// import menu_icon from '../../assets/menu-icon.png';
-// import { Link } from 'react-scroll';
-// import { NavLink } from 'react-router-dom'; // Import NavLink for routing
-
-// function Navbar() {
-//     const [sticky, setSticky] = useState(false);
-
-//     useEffect(() => {
-//         window.addEventListener('scroll', () => {
-//             window.scrollY > 50 ? setSticky(true) : setSticky(false);
-//         });
-//     }, []);
-
-//     const [mobileMenu, setMobileMenu] = useState(false);
-
-//     const toggleMenu = () => {
-//         setMobileMenu(prevState => !prevState);
-//     };
-
-//     return (
-//         <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-//             <img src={logo} alt="" className="logo" />
-//             <ul className={mobileMenu ? '' : 'hide-mobile-menu'}>
-//                 <li>
-//                     <Link to="hero" smooth={true} offset={0} duration={500}>
-//                         Home
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link to="program" smooth={true} offset={-260} duration={500}>
-//                         Program
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link to="about" smooth={true} offset={-150} duration={500}>
-//                         About us
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link to="campus" smooth={true} offset={-260} duration={500}>
-//                         Partners
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link to="testimonials" smooth={true} offset={-260} duration={500}>
-//                         Testimonials
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <Link to="contact" smooth={true} offset={-260} duration={500} className="btn">
-//                         Contact us
-//                     </Link>
-//                 </li>
-//                 <li>
-//                     <NavLink to="/login" className="btn">
-//                         Login
-//                     </NavLink>
-//                 </li>
-//             </ul>
-//             <img src={menu_icon} alt="" className="menu-icon" onClick={toggleMenu} />
-//         </nav>
-//     );
-// }
-
-// export default Navbar;
-
-
-
-
-
-
-
-
-
-
-// import React, {useEffect, useState} from 'react'
-// import './Navbar.css'
-// import logo from '../../assets/logo.png'
-// import menu_icon from '../../assets/menu-icon.png'
-// import { Link } from 'react-scroll';
-
-
-// function Navbar  () {
-
-//     const [sticky, setSticky] = useState(false);
-
-//     useEffect(()=>{
-//       window.addEventListener('scroll',()=>{
-//         window.scrollY > 50 ? setSticky(true) : setSticky(false);
-//       })
-//     },[])
-
-//     const [mobileMenu, setMobileMenu] = useState(false);
-//     const toggleMenu = ()=>{
-//       mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
-//     }
-
-//   return (
-//     <nav className={`container ${sticky? 'dark-nav' : ''}`}>
-//         <img src={logo} alt="" className='logo' />
-//         <ul className={mobileMenu?'':'hide-mobile-menu'}>
-
-//             <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
-
-//             <li><Link to='program' smooth={true} offset={-260} duration={500}>Program</Link></li>
-
-//             <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
-
-//             <li><Link to='campus' smooth={true} offset={-260} duration={500}>Partners</Link></li>
-
-//             <li><Link to='testimonials' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
-
-//             <li><Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>Contact us</Link></li>
-
-//         </ul>
-
-//         <img src={menu_icon} alt="" className='menu-icon' onClick={toggleMenu}/>
-//     </nav>
-//   )
-// }
-
-// export default Navbar;
